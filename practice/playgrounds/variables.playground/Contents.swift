3d63c84d7b885f071d246adf93e004b6ee2421e9
Anderson Tagata <attagata@live.com>
2021-07-07 19:11:28 -0300

    Variables on playground

---
+++ practice/playgrounds/variables.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+// MARK: - Variables
+
+var greeting = "Hello, playground"
+
+// Use var for variables that can change ("mutable") and let for constants that can't change ("non-mutable").
+
+// Integers are "whole" numbers, i.e. numbers without a fractional component.
+var meaningOfLife: Int = 42
+
+// Floats are decimal-point numbers, i.e. numbers with a fractional component.
+var phi: Float = 1.618
+
+// Doubles are floating point numbers with double precision, i.e. numbers with a fractional component. Doubles are preferred over floats.
+let pi: Double = 3.14159265359
+
+// A String is a sequence of characters, like text.
+var message: String = "Hello World!"
+
+// A boolean can be either true or false. You use booleans for logical operations.
+var isLoggedIn: Bool = false
+
+// You can assign the result of an expression to a variable, like this:
+var result: Int = 1 + 2
+
+// An expression is programmer lingo for putting stuff like variables, operators, constants, functions, etc. together. Like a + b in this example:
+let a = 3
+let b = 4
+let c = a + b
+
+// Swift can determine the type (Int, Double, String, etc.) of a variable on its own. This is called type inference. In this example, the type of name is inferred to be String.
+var name = "Anderson Tagata"
+
+// Variables can be optional, which means it either contains a value or it's nil. Optionals make coding Swift safer and more productive. Here's an optional string:
+var optionalMessage: String?
+
+
